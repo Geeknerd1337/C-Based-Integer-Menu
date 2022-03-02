bdfe1b7810a471147b49bed9dd3cd559f24144fb
Josh Wilson <geeknerd1337@gmail.com>
2022-03-02 15:29:28 -0600

    Set Up Main Menu Loop

--- code/major1.c
+++ code/major1.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
+#include <stdbool.h>
 int main() {
-   // printf() displays the string inside quotation
-   printf("Hello, World!");
-   return 0;
+    bool running = true;
+    
+    //Initialize variable for our choice, make it a character for simplicities sake
+    char choice;
+
+    while(running){
+        //Initial Introduction
+        printf("\nWelcome to the Major 1 Integer Menu!\nPlease input one of the following commands.\n1- Count Leading Zeros\n2- Perform an Endian Swap\n");
+        printf("3- Rotate an Integer\n4- Parity\n5- Exit\nYour Input: ");
+        //Take the input of our choice
+        scanf("%c",&choice);
+
+        switch(choice){
+            case '5':
+                printf("\n Program Terminating... goodbye...\n");
+                return 0;
+            break;
+            default:
+            printf("\nYou have input invalid input, please try again...");
+        }
+    }
+    return 0;
 }
